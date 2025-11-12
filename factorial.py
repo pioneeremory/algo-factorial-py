08ad562c16f36d4de87565488f7c79074cab62fb
logan <blacksuitphoto@gmail.com>
2025-11-12 14:53:41 -0800

    py factorial

--- factorial.py
+++ factorial.py
@@ -1,3 +1,19 @@
+# In this exercise you are going to create a function that takes a number parameter and returns the factorial 
+# of it.
+# A factorial is the result of multiplying a sequence of descending numbers down to 0. Factorials are only defined for non-negative integer 
+# numbers. This definition includes zero. Though 0! is equal to 1, so treat it as an edge case.
+
+# factorial(4) # => 24 (4 * 3 * 2 * 1)
+
 def factorial(num):
-	# your code here
-	pass
+    answer = 0
+    i = num
+    while i > 1:
+        answer = num * (i -1)
+        num = answer
+        i-= 1
+    
+    return answer
+print(factorial(6))
+
+pass
